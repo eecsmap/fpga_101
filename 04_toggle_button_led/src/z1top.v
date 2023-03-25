cc5ffd621aa43c6bae0e06930efc84af3e60c143
Wenyang Duan <wenyang.duan@gmail.com>
2023-03-24 22:41:06 -0700

    04_toggle_button_led_version2

--- 04_toggle_button_led/src/z1top.v
+++ 04_toggle_button_led/src/z1top.v
@@ -1,14 +1,16 @@
 module z1top(
+    input CLK_125MHZ_FPGA,
     input BUTTON,
     output LED
 );
 
     reg is_on = 0;
 
-    // we naively assume that the button is not bouncing
-    // yet this still cause warning/error in Vivado, addressed in constraints
-    always @(posedge BUTTON) begin
-        is_on <= ~is_on;
+    // this solution does not work well, but it is interesting to analyze the behavior
+    // if you hold the button down, the LED will half way light up (it frequently toggles on and off which you cannot tell)
+    // and when you release the button, the LED will stay on or off depending on the last state sampled by clock
+    always @(posedge CLK_125MHZ_FPGA) begin
+        is_on <= BUTTON ? ~is_on : is_on;
     end
 
     assign LED = is_on;
--- 04_toggle_button_led/src/z1top.xdc
+++ 04_toggle_button_led/src/z1top.xdc
@@ -1,3 +1,6 @@
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets BUTTON_IBUF]
+# Clock signal 125 MHz
+set_property -dict { PACKAGE_PIN H16   IOSTANDARD LVCMOS33 } [get_ports { CLK_125MHZ_FPGA }]; #IO_L13P_T2_MRCC_35 Sch=sysclk
+create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports { CLK_125MHZ_FPGA }];
+
 set_property -dict {PACKAGE_PIN D19 IOSTANDARD LVCMOS33} [get_ports {BUTTON}]
 set_property -dict {PACKAGE_PIN R14 IOSTANDARD LVCMOS33} [get_ports {LED}]
