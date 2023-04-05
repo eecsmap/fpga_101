9384d29b81a3708f72dc72dec0d7d0747443eb4f
Wenyang Duan <wenyang.duan@gmail.com>
2023-04-05 15:59:11 -0700

    normal logic

--- 05_uart/src/z1top.v
+++ 05_uart/src/z1top.v
@@ -2,42 +2,47 @@ module z1top(
     input sysclk,
     input data_ready,
     //input [7:0] data,
-    output busy, // sending existing data, not accepting new data
+    output reg busy, // sending existing data, not accepting new data
     output UART_TX
 );
     localparam DATA = 8'b01000001; // A
-
     localparam MAX_BIT_SENT = 10;
     localparam SYS_CLK_FREQ = 125000000;
     localparam BAUD_RATE = 115200;
-    localparam BAUD_LENGTH_IN_CYCLES = 3;//SYS_CLK_FREQ / BAUD_RATE;
+    localparam BAUD_LENGTH_IN_CYCLES = SYS_CLK_FREQ / BAUD_RATE;
 
-    reg [$clog2(BAUD_LENGTH_IN_CYCLES)-1:0] count;
-    reg [3:0] bit_sent; // wide enough to hold MAX_BIT_SENT
+    reg [$clog2(BAUD_LENGTH_IN_CYCLES)-1:0] count = 0;
     reg [9:0] shifting_data;
-
-    localparam STATE_IDLE = 1'b0;
-    localparam STATE_BUSY = 1'b1;
-    reg state = STATE_IDLE;
-
-    wire baud_end = state == STATE_BUSY && count == BAUD_LENGTH_IN_CYCLES - 1 ? 1 : 0; // level signal
-
-    wire [$clog2(BAUD_LENGTH_IN_CYCLES)-1:0] next_count = state == STATE_BUSY ? count == BAUD_LENGTH_IN_CYCLES - 1 ? 0 : count + 1 : 0;
-
-    wire [3:0] next_bit_sent = state == STATE_BUSY ? baud_end ? bit_sent == MAX_BIT_SENT - 1 ? 0 : bit_sent + 1 : bit_sent : 0;
-
-    wire [9:0] next_data = state == STATE_BUSY ? baud_end ? all_sent ? shifting_data : shifting_data >> 1 : shifting_data >> 1 : { 1'd1, DATA, 1'd0 };
-    wire all_sent = state == STATE_BUSY && baud_end && bit_sent == MAX_BIT_SENT - 1 ? 1 : 0;
-    wire next_state = state == STATE_IDLE ? data_ready : !all_sent;
+    reg [3:0] bit_sent = 0;
 
     always @(posedge sysclk) begin
-        count <= next_count;
-        bit_sent <= next_bit_sent;
-        shifting_data <= next_data;
-        state <= next_state;
+        // same question here, to avoid latch?
+        if (busy) begin
+            count <= count + 1;
+            // Q: do I need to say shifting_data <= shifting_data here?
+            // do I need to set count, shifting_data, bit_sent and busy here to avoid latch?
+            if (count == BAUD_LENGTH_IN_CYCLES - 1) begin
+                count <= 0;
+                shifting_data <= shifting_data >> 1;
+                if (bit_sent == MAX_BIT_SENT - 1) begin
+                    busy <= 0;
+                    bit_sent <= 0;
+                end else begin
+                    // Q: do I need to say busy <= 1 here?
+                    bit_sent <= bit_sent + 1;
+                end
+            end
+        end else begin
+            // Q: same question here
+            if (data_ready) begin // only check data_ready when not busy
+                shifting_data <= { 1'd1, DATA, 1'd0 }; // start_bit as 0, stop_bit as 1
+                busy <= 1;
+                count <= 0;
+                bit_sent <= 0;
+            end
+        end
     end
 
-    assign UART_TX = state == STATE_IDLE ? 1'b1 : shifting_data[0];
-    assign busy = state;
+    assign UART_TX = busy ? shifting_data[0] : 1'b1;
 
 endmodule
