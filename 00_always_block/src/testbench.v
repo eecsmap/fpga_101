c51b62197b7ebe8daafb01c6d7530fea4332b117
Wenyang Duan <wenyang.duan@gmail.com>
2023-12-15 00:58:18 -0800

    update always

--- 00_always_block/src/testbench.v
+++ 00_always_block/src/testbench.v
@@ -16,6 +16,29 @@ module testbench();
         $display("time: %t, in: %d, out: %d", $time, in, out);
     end
 
+    // time:                    2, in:   1, out:   0
+    // time:                    2, in:   1, out:   2
+    // time:                    4, in:   2, out:   2
+    // time:                    4, in:   2, out:   3
+    // time:                    6, in:   3, out:   3
+    // time:                    6, in:   3, out:   4
+    // time:                    8, in:   4, out:   4
+    // time:                    8, in:   4, out:   5
+    // time:                   10, in:   5, out:   5
+    // time:                   10, in:   5, out:   6
+    // time:                   12, in:   6, out:   6
+    // time:                   12, in:   6, out:   7
+    // time:                   14, in:   7, out:   7
+    // time:                   14, in:   7, out:   8
+    // time:                   16, in:   8, out:   8
+    // time:                   16, in:   8, out:   9
+    // this is because at time 2, first in change, and it prints 1, 0
+    // then at the same cycle, out is updated to 2, then it prints 1, 2
+    // always @(*) begin
+    //     out <= in + 1;
+    //     $display("time: %t, in: %d, out: %d", $time, in, out);
+    // end
+
     integer i, j;
     initial begin
         $dumpfile("testbench.fst");
